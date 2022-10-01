0c895d40dc78407e65b26720c4139cf29543a72d
rominaPP <108702781+rominaPP@users.noreply.github.com>
2022-09-30 23:41:51 -0300

    Primer commit
    Creación proyecto Spring Boot y base de datos

---
+++ Backend/src/main/java/com/portfolio/PerezR/PerezRApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.PerezR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerezRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerezRApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/portfolio/PerezR/PerezRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.PerezR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerezRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
