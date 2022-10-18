b3361df57e3f0112c92ed54430317e02fd63b002
rominaPP <108702781+rominaPP@users.noreply.github.com>
2022-10-18 20:18:56 -0300

    commit
    commit

--- Frondend/src/app/app.module.ts
+++ Frondend/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedesComponent } from './components/redes/redes.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LogoAPComponent,
-    RedesComponent,
-    BannerComponent,
-    AcercaDeMiComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
