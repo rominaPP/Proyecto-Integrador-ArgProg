3365bd77a9a90a796b2435d67c880286f1c2a0a0
rominaPP <108702781+rominaPP@users.noreply.github.com>
2022-10-18 21:21:49 -0300

    Stotage
    Instalación de la biblioteca de Firebase-Storage

--- Frondend/src/environments/environment.ts
+++ Frondend/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'backendromiperez',
+    appId: '1:33580507421:web:8dcd2988e04b97e66001f4',
+    storageBucket: 'backendromiperez.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCK4vWoJYqI3r5xslan5lPDq5PnQj6c-yY',
+    authDomain: 'backendromiperez.firebaseapp.com',
+    messagingSenderId: '33580507421',
+  },
   production: false
 };
 
