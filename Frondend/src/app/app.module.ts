24c06d962f002f12cba0e1c8f4209bc012d1cbfc
rominaPP <108702781+rominaPP@users.noreply.github.com>
2022-10-18 22:47:46 -0300

    Header
    Creación del componente Header

--- Frondend/src/app/app.module.ts
+++ Frondend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
